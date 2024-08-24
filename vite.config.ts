1574f7a068eaf68bb2f5339188330d19fe60af19
viperluan <viperluan@gmail.com>
2024-08-24 19:27:59 -0300

    adiciona alias iniciais

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+      '~api': path.resolve(__dirname, './src/api'),
+      '~assets': path.resolve(__dirname, './src/assets'),
+      '~components': path.resolve(__dirname, './src/components'),
+      '~context': path.resolve(__dirname, './src/context'),
+      '~layouts': path.resolve(__dirname, './src/layouts'),
+      '~views': path.resolve(__dirname, './src/views'),
+    }
+  },
 })
