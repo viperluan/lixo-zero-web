c9787149422abb7236861672f0e611504165eec6
viperluan <viperluan@gmail.com>
2024-08-24 19:27:09 -0300

    modifica main para tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
 
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import "assets/plugins/nucleo/css/nucleo.css";
+import "~assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-dashboard-react.scss";
+import "~assets/scss/argon-dashboard-react.scss";
 
-import AdminLayout from "layouts/Admin.js";
-import AuthLayout from "layouts/Auth.js";
-import HomeLayout from "layouts/Home";
+import AdminLayout from "~layouts/Admin";
+import AuthLayout from "~layouts/Auth";
+import HomeLayout from "~layouts/Home";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from '~context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <Fragment>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <AuthProvider>
       <ToastContainer
         position="top-right"
@@ -39,5 +38,5 @@ root.render(
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  </Fragment>
-);
+  </StrictMode>,
+)
