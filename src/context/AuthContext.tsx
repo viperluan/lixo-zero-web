07934d5e6941660c40d870d58f7137079affeda9
viperluan <viperluan@gmail.com>
2024-08-28 10:48:59 -0300

    atualiza auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useState, useEffect, PropsWithChildren } from 'react';
+import { useCookies } from 'react-cookie';
+import * as jose from 'jose';
+import api from '~/api';
+
+export type UserType = {
+  id: string;
+  nome: string;
+  email: string;
+  tipo: string;
+};
+
+export type AuthContextType = {
+  user: UserType | null;
+  loading: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<UserType | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+  useEffect(() => {
+    const loadUserData = () => {
+      const token = cookies['token'];
+
+      if (token) {
+        login(token);
+      }
+
+      setLoading(false);
+    };
+
+    loadUserData();
+  }, []);
+
+  const login = (token: string) => {
+    setCookie('token', token);
+
+    const userPayloadFromToken = decodeToken(token);
+    setUser(userPayloadFromToken);
+
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  };
+
+  const logout = () => {
+    removeCookie('token');
+    setUser(null);
+  };
+
+  const decodeToken = (token: string): UserType => {
+    const { id, email, nome, tipo } = jose.decodeJwt<UserType>(token);
+
+    return { id, email, nome, tipo };
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+};
