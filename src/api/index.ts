6cff96f158afefd96d328c2859a14d81dd2d66ac
viperluan <viperluan@gmail.com>
2024-08-28 10:48:44 -0300

    atualiza api

---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+
+type ErrorResponseType = {
+  message?: string;
+  error?: string;
+};
+
+const environment: string = import.meta.env.NODE_ENV || '';
+
+const api = axios.create({
+  baseURL: environment,
+  timeout: 10000, // 10 segundos
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  validateStatus: (status) => status < 500,
+});
+
+const responseSuccessInterceptor = (response: AxiosResponse) => response;
+
+const responseErrorInterceptor = (error: AxiosError<ErrorResponseType>) => {
+  if (error.response) {
+    const serverErrorMessage = error.response.data?.message || error.response.data?.error;
+    toast.error(serverErrorMessage || 'Ocorreu um erro no servidor');
+
+    return Promise.reject(error);
+  }
+
+  toast.error('Erro de conexão ou timeout');
+  return Promise.reject('Erro de conexão ou timeout');
+};
+
+api.interceptors.response.use(responseSuccessInterceptor, responseErrorInterceptor);
+
+export default api;
