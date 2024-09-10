38ad3083059007c9019dfb9d0de74cba1acfa99f
viperluan <viperluan@gmail.com>
2024-09-10 02:11:46 -0300

    reordena imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-
 import '~assets/plugins/nucleo/css/nucleo.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '~assets/scss/argon-dashboard-react.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import AdminLayout from '~layouts/Admin';
 import AuthLayout from '~layouts/Auth';
 import HomeLayout from '~layouts/Home';
+
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from '~context/AuthContext';
 import { CookiesProvider } from 'react-cookie';
 
