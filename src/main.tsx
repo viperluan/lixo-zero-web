77d98c8c35c2f5288fc9f3aa4c4a0548472de6c1
viperluan <viperluan@gmail.com>
2024-09-15 10:49:45 -0300

    adiciona import de estilo do bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import '~assets/plugins/nucleo/css/nucleo.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '~assets/plugins/nucleo/css/nucleo.css';
 import '~assets/scss/argon-dashboard-react.scss';
+
 import 'react-toastify/dist/ReactToastify.css';
 import './styles.css';
 
