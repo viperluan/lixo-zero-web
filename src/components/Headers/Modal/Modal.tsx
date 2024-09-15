846b02ae170bd4f75fb9693205c14a943addf7c2
viperluan <viperluan@gmail.com>
2024-09-15 14:11:04 -0300

    corrige modal de login

--- src/components/Headers/Modal/Modal.tsx
+++ src/components/Headers/Modal/Modal.tsx
@@ -1,25 +1,24 @@
-import { Fragment, PropsWithChildren, useState } from 'react';
+import { PropsWithChildren } from 'react';
 import {
   Modal,
-  Form,
   FormGroup,
   Input,
   Button,
-  CardBody,
-  Card,
   InputGroup,
   InputGroupAddon,
   InputGroupText,
-  CardFooter,
   ModalHeader,
+  ModalFooter,
+  ModalBody,
 } from 'reactstrap';
 import '../../../assets/scss/argon-dashboard/custom/modals/_modal.scss';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import api from '../../../api';
 import { useAuth } from '~context/AuthContext';
-import { LoadingOverlay } from '~components/Loading';
-import { AxiosResponse } from 'axios';
+
+import { Formik, Form, FormikHelpers } from 'formik';
+import * as yup from 'yup';
 
 type UserAuthenticateResponseType = {
   token: string;
@@ -32,106 +31,136 @@ type UserAuthenticateResponseType = {
   error?: string;
 };
 
+type UserType = {
+  email: string;
+  password: string;
+};
+
 interface IModalLoginProps extends PropsWithChildren {
   isOpen: boolean;
   toggle: () => void;
 }
 
 const ModalLogin = ({ isOpen, toggle }: IModalLoginProps) => {
-  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const { login } = useAuth();
   const navigate = useNavigate();
+
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Formato de email inválido.')
+      .required('É necessário informar um email.'),
+    password: yup.string().required('É necessário informar uma senha.'),
+  });
 
   const handleClick = () => {
     navigate('/auth/register');
     toggle();
   };
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const handleSubmit = async (values: UserType, actions: FormikHelpers<UserType>) => {
+    const { email, password } = values;
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  const validate = async () => {
     try {
-      if (!emailRegex.test(email)) return toast.error('Informe um e-mail válido.');
+      const { data } = await api.post<UserAuthenticateResponseType>('/usuarios/autenticar', {
+        email,
+        senha: password,
+      });
 
-      if (!password) return toast.error('Informe a senha!');
+      if (data.token) {
+        login(data.token);
+        toast.success(`Usuario ${data.usuario?.nome} autenticado!`);
+        actions.resetForm();
+        navigate(location.pathname);
+        toggle();
+      }
 
-      setIsLoading(true);
+      if (data.error) toast.error(data.error);
 
-      await api
-        .post('/usuarios/autenticar', { email, senha: password })
-        .then((response: AxiosResponse<UserAuthenticateResponseType>) => {
-          if (response.data.token) {
-            login(response.data.token);
-            toast.success(`Usuario ${response.data.usuario?.nome} autenticado!`);
-            navigate(location.pathname);
-            toggle();
-          }
-
-          if (response.data.error) toast.error(response.data.error);
-        })
-        .finally(() => setIsLoading(false));
+      actions.setSubmitting(false);
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      /* empty */
+      toast.error(
+        'Ocorreu algum erro ao tentarmos fazer sua autenticação, tente novamente mais tarde.'
+      );
     }
   };
 
   return (
-    <Fragment>
-      <LoadingOverlay isLoading={isLoading} />
-      <Modal isOpen={isOpen} toggle={toggle} className="modal-box">
-        <ModalHeader toggle={toggle}>Faça o login</ModalHeader>
-        <Card className="bg-secondary shadow border-0">
-          <CardBody>
+    <>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ handleChange, handleBlur, values, errors, touched, isSubmitting }) => (
+          <Modal isOpen={isOpen} toggle={toggle} className="modal-box">
             <Form className="form">
-              <FormGroup className="mb-3">
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-email-83" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-lock-circle-open" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Senha"
-                    type="password"
-                    autoComplete="new-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </InputGroup>
-              </FormGroup>
+              <ModalHeader toggle={toggle}>Faça o login</ModalHeader>
+
+              <ModalBody>
+                <FormGroup className="mb-3">
+                  <InputGroup className="">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+
+                    <Input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Digite seu email"
+                      autoComplete="email"
+                      style={{ paddingLeft: '12px' }}
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      invalid={touched.email && !!errors.email}
+                    />
+                  </InputGroup>
+                </FormGroup>
+
+                <FormGroup>
+                  <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+
+                    <Input
+                      id="password"
+                      name="password"
+                      placeholder="Digite sua senha"
+                      type="password"
+                      autoComplete="password"
+                      style={{ paddingLeft: '12px' }}
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      invalid={touched.password && !!errors.password}
+                    />
+                  </InputGroup>
+                </FormGroup>
+              </ModalBody>
+
+              <ModalFooter className="d-flex justify-content-center">
+                <Button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? 'Entrando...' : 'Entrar'}
+                </Button>
+
+                <Button onClick={handleClick} color="primary">
+                  Cadastrar
+                </Button>
+              </ModalFooter>
             </Form>
-          </CardBody>
-          <CardFooter className="d-flex justify-content-center">
-            <Button onClick={validate}>Entrar</Button>
-            <Button onClick={handleClick} color="primary">
-              Cadastrar
-            </Button>
-          </CardFooter>
-        </Card>
-      </Modal>
-    </Fragment>
+          </Modal>
+        )}
+      </Formik>
+    </>
   );
 };
 
