03699e0c50355550226932bc2c386b01942fc276
viperluan <viperluan@gmail.com>
2024-10-01 14:52:51 -0300

    corrige bug de numero máximo de digitos para cnpj

--- src/components/Inputs/InputCpfCnpj.jsx
+++ src/components/Inputs/InputCpfCnpj.jsx
@@ -3,20 +3,21 @@ import { Input } from 'reactstrap';
 
 const InputCpfCnpj = ({ value, onChange, ...props }) => {
   const applyDisplayMask = (rawValue) => {
-    const onlyNums = rawValue.slice(0, 13); // limita o máximo de dígitos
+    const onlyNums = rawValue.slice(0, 14); // limita o máximo de dígitos
+
     if (onlyNums.length <= 11) {
       return onlyNums.replace(
         /(\d{3})(\d{1,3})?(\d{1,3})?(\d{1,2})?/,
         (_, $1, $2, $3, $4) =>
           `${$1}${$2 ? '.' + $2 : ''}${$3 ? '.' + $3 : ''}${$4 ? '-' + $4 : ''}`
       );
-    } else {
-      return onlyNums.replace(
-        /(\d{2})(\d{1,3})?(\d{1,3})?(\d{1,4})?(\d{1,2})?/,
-        (_, $1, $2, $3, $4, $5) =>
-          `${$1}${$2 ? '.' + $2 : ''}${$3 ? '.' + $3 : ''}${$4 ? '/' + $4 : ''}${$5 ? '-' + $5 : ''}`
-      );
     }
+
+    return onlyNums.replace(
+      /(\d{2})(\d{1,3})?(\d{1,3})?(\d{1,4})?(\d{1,2})?/,
+      (_, $1, $2, $3, $4, $5) =>
+        `${$1}${$2 ? '.' + $2 : ''}${$3 ? '.' + $3 : ''}${$4 ? '/' + $4 : ''}${$5 ? '-' + $5 : ''}`
+    );
   };
 
   const handleChange = (e) => {
